d4c5bf056d6a106534a81a5d741e6c3f891571a9
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-23 14:59:59 -0400

    match spider iter flow complete

--- main/index.ts
+++ main/index.ts
@@ -13,138 +13,6 @@ import { findEntry } from "./findEntry";
 config();
 log.setLevel(log.levels.DEBUG);
 
-let { RIOT_API_NA, RIOT_API_KEY, FALLBACK_ENTRY } = process.env;
-
-if (
-  RIOT_API_NA === undefined ||
-  RIOT_API_KEY === undefined ||
-  FALLBACK_ENTRY === undefined
-) {
-  throw "no key or url or entry";
-}
-axios.defaults.headers.common["X-Riot-Token"] = RIOT_API_KEY;
-
-const MONGO_URI = "mongodb://localhost:20000";
-
-const leagueDB = "league";
-const matchesColl = "matches";
-const timelineColl = "timelines";
-const analysisColl = "analysis";
-
-export async function matchSpider(RIOT_API_REGION: string) {
-  const FEATURED_GAMES_URL = "";
-
-  // init MatchBuffer
-  const matchBuffer = new MatchBuffer(1000);
-  let client: Mongo;
-  try {
-    // connect to Mongo.
-    log.debug(`Connecting to mongoDb at ${MONGO_URI}`);
-    client = await Mongo.connect(MONGO_URI, { useUnifiedTopology: true });
-    log.info(`Connected to mongoDb at ${MONGO_URI}`);
-    // connect to db
-    let League = client.db(leagueDB);
-    // connect to collection
-    let Matches = League.collection<Match>(matchesColl);
-    let Timelines = League.collection<Frames>(timelineColl);
-    log.info("Connected to collections: matches, timelines, analysis");
-
-    log.info("Beginning Crawl");
-    while (true) {
-      // shift most recent game in buffer and get match data
-      // if none then find new entry
-      let targetMatch = matchBuffer.shift();
-      let targetGameId =
-        targetMatch?.gameId ||
-        (await findEntry(Matches, RIOT_API_REGION, FEATURED_GAMES_URL));
-      log.debug("matchFrom Buffer:", targetMatch?.gameId, targetMatch?.queue);
-      if (
-        targetMatch &&
-        targetMatch?.queue !== QueueID.Solo_SR &&
-        targetMatch?.queue !== QueueID.Flex_SR
-      ) {
-        // if match isn't ranked: skip
-        log.debug(`targetMatch: ${targetMatch.gameId} is not Ranked; skipping`);
-        continue;
-      } else if (await existsInCollection(Matches)({ gameId: targetGameId })) {
-        // if match is already in db: skip
-        log.debug(`${targetGameId} is already in Db; skipping`);
-        continue;
-      }
-
-      // fetch match and timeline
-      let [matchRes, timelineRes] = await fetchMatchAndTimeline(
-        targetGameId,
-        RIOT_API_REGION
-      );
-
-      // if match is less than 15 minutes: continue to next match;
-      if (matchRes.data.gameDuration < 900) {
-        log.debug(
-          `match ${targetGameId} is too short; game duration: ${matchRes.data.gameDuration}`
-        );
-        continue;
-      }
-
-      // add gameId to timeline
-      let timeline: Frames = Object.assign({}, timelineRes.data, {
-        gameId: matchRes.data.gameId,
-      });
-
-      // TODO attach role analysis results to match
-
-      try {
-        // insert into database
-        await Promise.all([
-          Timelines.insertOne(timeline),
-          Matches.insertOne(matchRes.data),
-        ]);
-        log.info(`New Entry: `, matchRes.data.gameId);
-      } catch (err) {
-        // catch and log any errors. usually duplicate key error
-        // TODO: error handling for insertions
-        log.warn("Error writing to DB", err);
-        // but just log it and go to next
-      }
-
-      // pick a random account from list of participants in target match
-      if (matchBuffer.length < matchBuffer.max_size - 10) {
-        let randomAccount =
-          matchRes.data.participantIdentities[Math.floor(Math.random() * 10)]
-            .player.accountId;
-        try {
-          // get match history of randomAccount
-          let matchHistory = await fetchMatchHistory(
-            randomAccount,
-            RIOT_API_REGION
-          );
-          // push match history to buffer
-          matchBuffer.push(
-            ...matchHistory.data.matches.filter((summary) => {
-              return (
-                summary.queue === QueueID.Solo_SR ||
-                summary.queue === QueueID.Flex_SR
-              );
-            })
-          );
-          log.debug(`matchBuffer length is now: ${matchBuffer.length}`);
-        } catch (err) {
-          // if theres an error fetching just skip and go next
-          log.info("Error fetching matchHistory for buffer going next", err);
-        }
-      }
-    }
-  } catch (err) {
-    // if error connecting to db log error then exit
-    log.error("Fatal Error. Ending Processes");
-    log.trace(err);
-  }
-
-  // @ts-ignore
-  if (client !== undefined) {
-    client.close();
-  }
-}
 
 type MatchSpiderOptions = CommonOptions &
   (AccountFallback | FeaturedGameFallback);
@@ -157,7 +25,8 @@ interface CommonOptions {
   bufferSize?: number;
   queues?: QueueID[];
   entryGameId?: string;
-  duplicateChecker?: (gameId: number) => boolean | boolean;
+  duplicateChecker?: (gameId: number) => (boolean | Promise<boolean>);
+  max_iter?: number;
 }
 
 interface AccountFallback {
@@ -216,10 +85,12 @@ export function MatchSpider(options: MatchSpiderOptions) {
   // load from dot env for access in functions
   config();
   if (process.env.RIOT_API_KEY) {
+    // log successfully found
     log.info("Using API key:", process.env.RIOT_API_KEY);
   } else {
-    log.warn("RIOT_API_KEY not found in .env");
-    throw "RIOT_API_KEY not found in .env";
+    // 
+    log.warn("RIOT_API_KEY not found in .env; terminating");
+    throw "RIOT_API_KEY not found in .env; add RIOT_API_KEY to .env and try again";
   }
 
   const FREATURED_GAMES_URL = "";
@@ -230,7 +101,7 @@ export function MatchSpider(options: MatchSpiderOptions) {
     max_attempts: 3,
     max_age: 24 * 60 * 60 * 1000,
     entryGameId: undefined,
-    duplicateChecker: () => false,
+    duplicateChecker: async function(){return false},
   };
 
   let _options = Object.assign(defaults, options);
@@ -258,9 +129,34 @@ export function MatchSpider(options: MatchSpiderOptions) {
             FREATURED_GAMES_URL,
             _options.max_age
           ));
-        // get the game
 
-        yield 3;
+        // check if match is a duplicate or not
+        if (await _options.duplicateChecker(targetMatch.gameId)) {
+          // if is duplicate skip
+          continue
+        } 
+        
+        // get the game info
+        let [matchRes, timelineRes] = await fetchMatchAndTimeline(targetMatch.gameId, RegionLookup[_options.region])
+
+
+        // get a random player to pull match history from
+        let randomAccount =
+          matchRes.data.participantIdentities[Math.floor(Math.random() * 10)]
+            .player.accountId;
+
+        let matchHistory = await fetchMatchHistory(randomAccount, RegionLookup[_options.region])
+        matchHistory.data.matches.forEach(match=>{
+          if (match.queue in _options.queues) {
+            matchBuffer.push(match)
+          }
+        })
+
+        // yield result
+        yield {
+          match: matchRes.data,
+          timeline: timelineRes.data
+        }
         // {
         //   Match,
         //   Frames,

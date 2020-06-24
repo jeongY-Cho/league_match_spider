d3cf7c64cc364b820333dcf1ad43286c2e4f4cae
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-24 13:19:30 -0400

    made code more dry and also wrote tests

---
+++ main/__tests__/findEntry.test.ts
@@ -0,0 +1,95 @@
+import { findEntry } from "../findEntry";
+import axios from "axios";
+import { mocked } from "ts-jest/utils";
+import { ValueOfRegions } from "..";
+import { QueueID } from "../types";
+
+jest.mock("axios");
+
+
+let now = Date.now()
+mocked(axios.get).mockResolvedValue({
+  data: {
+    matches: [
+      {timestamp: now, queue: QueueID.ARAM_ButchersBridge},
+      {timestamp: now - 1001, queue: QueueID.Flex_SR}
+    ],
+    gameList: [{ participants: [
+      {summonerName: "abc"}
+    ] }],
+    accountId: "abc",
+    participantIdentities: [{
+      player: {
+        accountId: 123
+      }
+    }],
+  },
+});
+
+test("test without id", async () => {
+
+  let res = await findEntry(
+    undefined,
+    "https://test1.api.riotgames.com" as ValueOfRegions,
+    [QueueID.Flex_SR]
+  );
+  expect(axios.get).toBeCalledTimes(3);
+  expect(res).toMatchObject({
+    timestamp: now - 1001, 
+    queue: QueueID.Flex_SR
+  })
+});
+
+test("test with id", async () => {
+  jest.clearAllMocks()
+  let res = await findEntry(
+    123,
+    "https://test1.api.riotgames.com" as ValueOfRegions,
+    [QueueID.Flex_SR]
+  );
+  expect(axios.get).toBeCalledTimes(2);
+  expect(res).toMatchObject({
+    timestamp: now - 1001,
+    queue: QueueID.Flex_SR,
+  });
+});
+
+test("test non default max_age", async () => {
+  let spyNow = jest.spyOn(global.Date, "now").mockReturnValue(now)
+  jest.clearAllMocks()
+  let res = await findEntry(
+    123,
+    "https://test1.api.riotgames.com" as ValueOfRegions,
+    [ QueueID.Flex_SR  ],
+    0
+  );
+  expect(axios.get).toBeCalledTimes(2);
+  expect(res).toBeUndefined()
+});
+test("test queue filter", async () => {
+  let spyNow = jest.spyOn(global.Date, "now").mockReturnValue(now)
+  jest.clearAllMocks()
+  let res1 = await findEntry(
+    123,
+    "https://test1.api.riotgames.com" as ValueOfRegions,
+    [ QueueID.Flex_SR  ],
+    
+  );
+  expect(axios.get).toBeCalledTimes(2);
+  expect(res1).toMatchObject({
+    timestamp: now - 1001,
+    queue: QueueID.Flex_SR
+  })
+
+  jest.clearAllMocks()
+  let res2 = await findEntry(
+    123,
+    "https://test1.api.riotgames.com" as ValueOfRegions,
+    [ QueueID.ARAM_ButchersBridge  ],
+    
+  );
+  expect(axios.get).toBeCalledTimes(2);
+  expect(res2).toMatchObject({
+    timestamp: now,
+    queue: QueueID.ARAM_ButchersBridge,
+  });});
--- main/findEntry.ts
+++ main/findEntry.ts
@@ -1,38 +1,46 @@
 import { fetchMatch } from "./fetchers/fetchMatch";
 import { fetchMatchHistory } from "./fetchers/fetchMatchHistory";
 import { ValueOfRegions } from ".";
-import { MatchSummary, Match, QueueID } from "./types";
-import { AxiosResponse } from "axios";
+import { MatchSummary, QueueID } from "./types";
 import { fetchFeaturedMatches } from "./fetchers/fetchFeaturedMatches";
 import { fetchAccountInfo } from "./fetchers/fetchAccountInfo";
 
 export async function findEntry(
   entryGameId: number | undefined,
   RIOT_API_REGION: ValueOfRegions,
+  queues: QueueID[],
   max_age = 48 * 60 * 60 * 1000,
-  queues: QueueID[]
 ): Promise<MatchSummary> {
-  let match: AxiosResponse<Match>;
+  let randomParticipantId: string;
+
   if (entryGameId) {
     // if entry game id: then get the match from that
-    match = await fetchMatch(entryGameId, RIOT_API_REGION);
-    let randomParticipantId =
+    let match = await fetchMatch(entryGameId, RIOT_API_REGION);
+    randomParticipantId =
       match.data.participantIdentities[
-        Math.floor(Math.random() * match.data.participants.length)
-      ].player.accountId
-      
-      return (await fetchMatchHistory(randomParticipantId, RIOT_API_REGION)  ).data.matches[0]
+        Math.floor(Math.random() * match.data.participantIdentities.length)
+      ].player.accountId;
   } else {
     // if featured games:
     //    then get a list of featured games from api
     let featuredGames = await fetchFeaturedMatches(undefined, RIOT_API_REGION);
-    let matchesSummary = featuredGames.data.gameList.filter((match) => {
-      return match.gameQueueConfigID in queues
-    });
-    
-    let randomGame = matchesSummary[Math.floor(Math.random() * (matchesSummary.length))]
-    let randomParticipant = randomGame.participants[Math.floor(Math.random() * randomGame.participants.length)]
-    let randomParticipantAccountInfo = await fetchAccountInfo(randomParticipant.summonerName, RIOT_API_REGION)
-    return (await fetchMatchHistory(randomParticipantAccountInfo.accountId, RIOT_API_REGION)  ).data.matches[0]
+    let matchesSummary = featuredGames.data.gameList
+
+    let randomGame =
+      matchesSummary[Math.floor(Math.random() * matchesSummary.length)];
+    let randomParticipant =
+      randomGame.participants[
+        Math.floor(Math.random() * randomGame.participants.length)
+      ];
+    randomParticipantId = (
+      await fetchAccountInfo(randomParticipant.summonerName, RIOT_API_REGION)
+    ).data.accountId;
   }
+
+  let matchHistory = (
+    await fetchMatchHistory(randomParticipantId, RIOT_API_REGION)
+  ).data.matches;
+  return matchHistory.filter((match) => {
+    return (match.timestamp >= Date.now() - max_age - 1000 && queues.includes(match.queue)) 
+  })[0];
 }

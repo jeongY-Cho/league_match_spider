2369ed8722fdb2fde12e302634034789cbeee52d
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-23 20:24:41 -0400

    implemented a constants file

---
+++ main/constants.ts
@@ -0,0 +1,4 @@
+export const endpoints = {
+         ACCOUNT_INFO: "/lol/summoner/v4/summoners/by-name/",
+         
+       };
--- main/fetchers/fetchAccountInfo.ts
+++ main/fetchers/fetchAccountInfo.ts
@@ -1,11 +1,12 @@
 import { fetchWrapper } from "./fetchWrapper";
+import {endpoints} from "../constants"
 import { ValueOfRegions } from "..";
 import url from "url"
 import Axios from "axios";
 import { AccountInfo } from "../types"
 
 function _fetchAccountInfo(id: string, REGION: ValueOfRegions): Promise<AccountInfo> {
-    let endpoint = url.resolve(REGION, "/lol/summoner/v4/summoners/by-name/" + id)
+    let endpoint = url.resolve(REGION, endpoints.ACCOUNT_INFO + id)
     return Axios.get(endpoint, {
         headers: {
             "X-Riot-Token": process.env.RIOT_API_KEY

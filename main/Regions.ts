04579750cbffc48c7f5b9a4d01894d057651df2b
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-23 21:55:30 -0400

    moved region definitions to separate file

---
+++ main/Regions.ts
@@ -0,0 +1,48 @@
+export enum Regions {
+  "BR1",
+  "EUN1",
+  "EUW1",
+  "JP1",
+  "KR",
+  "LA1",
+  "LA2",
+  "NA1",
+  "OC1",
+  "TR1",
+  "RU",
+  "AMERICAS",
+  "ASIA",
+  "EUROPE",
+}
+export type RegionLookup = {
+  [Regions.BR1]: "https://br1.api.riotgames.com";
+  [Regions.EUN1]: "https://eun1.api.riotgames.com";
+  [Regions.EUW1]: "https://euw1.api.riotgames.com";
+  [Regions.JP1]: "https://jp1.api.riotgames.com";
+  [Regions.KR]: "https://kr.api.riotgames.com";
+  [Regions.LA1]: "https://la1.api.riotgames.com";
+  [Regions.LA2]: "https://la2.api.riotgames.com";
+  [Regions.NA1]: "https://na1.api.riotgames.com";
+  [Regions.OC1]: "https://oc1.api.riotgames.com";
+  [Regions.TR1]: "https://tr1.api.riotgames.com";
+  [Regions.RU]: "https://ru.api.riotgames.com";
+  [Regions.AMERICAS]: "https://americas.api.riotgames.com";
+  [Regions.ASIA]: "https://asia.api.riotgames.com";
+  [Regions.EUROPE]: "https://europe.api.riotgames.com";
+};
+export const RegionLookup: RegionLookup = {
+  [Regions.BR1]: "https://br1.api.riotgames.com",
+  [Regions.EUN1]: "https://eun1.api.riotgames.com",
+  [Regions.EUW1]: "https://euw1.api.riotgames.com",
+  [Regions.JP1]: "https://jp1.api.riotgames.com",
+  [Regions.KR]: "https://kr.api.riotgames.com",
+  [Regions.LA1]: "https://la1.api.riotgames.com",
+  [Regions.LA2]: "https://la2.api.riotgames.com",
+  [Regions.NA1]: "https://na1.api.riotgames.com",
+  [Regions.OC1]: "https://oc1.api.riotgames.com",
+  [Regions.TR1]: "https://tr1.api.riotgames.com",
+  [Regions.RU]: "https://ru.api.riotgames.com",
+  [Regions.AMERICAS]: "https://americas.api.riotgames.com",
+  [Regions.ASIA]: "https://asia.api.riotgames.com",
+  [Regions.EUROPE]: "https://europe.api.riotgames.com",
+};
--- main/index.ts
+++ main/index.ts
@@ -6,6 +6,7 @@ import { fetchMatchHistory } from "./fetchers/fetchMatchHistory";
 import MatchBuffer from "./MatchBuffer";
 import { fetchMatchAndTimeline } from "./fetchers/fetchMatchAndTimeline";
 import { findEntry } from "./findEntry";
+import { Regions, RegionLookup } from "./Regions";
 config();
 log.setLevel(log.levels.DEBUG);
 
@@ -31,47 +32,6 @@ interface AccountFallback {
 interface FeaturedGameFallback {
   fallbackMethod?: "featured";
 }
-
-enum Regions {
-  "BR1",
-  "EUN1",
-  "EUW1",
-  "JP1",
-  "KR",
-  "LA1",
-  "LA2",
-  "NA1",
-  "OC1",
-  "TR1",
-  "RU",
-}
-
-type RegionLookup = {
-  [Regions.BR1]: "https://br1.api.riotgames.com";
-  [Regions.EUN1]: "https://eun1.api.riotgames.com";
-  [Regions.EUW1]: "https://euw1.api.riotgames.com";
-  [Regions.JP1]: "https://jp1.api.riotgames.com";
-  [Regions.KR]: "https://kr.api.riotgames.com";
-  [Regions.LA1]: "https://la1.api.riotgames.com";
-  [Regions.LA2]: "https://la2.api.riotgames.com";
-  [Regions.NA1]: "https://na1.api.riotgames.com";
-  [Regions.OC1]: "https://oc1.api.riotgames.com";
-  [Regions.TR1]: "https://tr1.api.riotgames.com";
-  [Regions.RU]: "https://ru.api.riotgames.com";
-};
-const RegionLookup: RegionLookup = {
-  [Regions.BR1]: "https://br1.api.riotgames.com",
-  [Regions.EUN1]: "https://eun1.api.riotgames.com",
-  [Regions.EUW1]: "https://euw1.api.riotgames.com",
-  [Regions.JP1]: "https://jp1.api.riotgames.com",
-  [Regions.KR]: "https://kr.api.riotgames.com",
-  [Regions.LA1]: "https://la1.api.riotgames.com",
-  [Regions.LA2]: "https://la2.api.riotgames.com",
-  [Regions.NA1]: "https://na1.api.riotgames.com",
-  [Regions.OC1]: "https://oc1.api.riotgames.com",
-  [Regions.TR1]: "https://tr1.api.riotgames.com",
-  [Regions.RU]: "https://ru.api.riotgames.com",
-};
 
 export type ValueOf<T> = T[keyof T];
 export type ValueOfRegions = ValueOf<RegionLookup>;

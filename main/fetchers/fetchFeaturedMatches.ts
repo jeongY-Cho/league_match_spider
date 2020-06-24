fd4828e1e20ba33918e4a3165eacccac965448b1
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-24 10:18:50 -0400

    rename type files

--- main/fetchers/fetchFeaturedMatches.ts
+++ main/fetchers/fetchFeaturedMatches.ts
@@ -2,17 +2,14 @@ import axios, { AxiosResponse } from "axios";
 import { fetchWrapper } from "./fetchWrapper";
 import { ValueOfRegions } from "..";
 import url from "url";
-import { FeaturedMatchResponse } from "../types";
+import {FeaturedMatchesResponse}  from "../types";
 import { endpoints } from "../constants";
 
 function _fetchFeaturedMatches(
   u: undefined,
   region: ValueOfRegions
-): Promise<AxiosResponse<FeaturedMatchResponse>> {
-  let featuredGamesEndpoint = url.resolve(
-    region,
-    endpoints.FEATURED_MATCHES
-  );
+): Promise<AxiosResponse<FeaturedMatchesResponse.FeaturedMatchResponse>> {
+  let featuredGamesEndpoint = url.resolve(region, endpoints.FEATURED_MATCHES);
   return axios.get(featuredGamesEndpoint, {
     headers: {
       "X-Riot-Token": process.env.RIOT_API_KEY!,
--- main/types/index.ts
+++ main/types/index.ts
@@ -1,5 +1,5 @@
 export { AccountInfo } from "./AccountInfo"
-export { BannedChampion,FeaturedMatchResponse,GameList,Observers,Participant } from "./FeaturedMatchesResponse";
+export * as FeaturedMatchesResponse from "./FeaturedMatchesResponse";
 export { Event,BuildingType,Frame,Frames,FramesResponse,LaneType,LevelUpType,ParticipantFrame,Position,Type,WardType } from "./FullTimeline"
 export { Ban,Match,Participant,ParticipantIdentity,PlatformID,Player,QueueID,Stats,Team,Timeline } from "./Match"
 export { Lane,MatchHistoryResponse,MatchSummary,Role } from "./MatchHistory"

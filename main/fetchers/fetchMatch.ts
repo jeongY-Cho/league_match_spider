09808c505d26f10984b2bcd8dd2ef9cfe567b1b6
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-23 20:56:06 -0400

    changed to inline tokens and wrote tests

--- main/fetchers/fetchMatch.ts
+++ main/fetchers/fetchMatch.ts
@@ -8,6 +8,10 @@ import { endpoints } from "../constants";
 
 function _fetchMatch(match: number, RIOT_API_REGION: ValueOfRegions) {
   const MATCH_ENDPOINT = url.resolve(RIOT_API_REGION, endpoints.MATCHES);
-  return axios.get<Match>(MATCH_ENDPOINT + match);
+  return axios.get<Match>(MATCH_ENDPOINT + match, {
+    headers: {
+      "X-Riot-Token": process.env.RIOT_API_KEY
+    }
+  });
 }
 export const fetchMatch = fetchWrapper(_fetchMatch);

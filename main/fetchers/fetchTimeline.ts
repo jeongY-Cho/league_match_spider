7db82df5032ec91ec2399256ec02e46b2850d6a6
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-25 23:17:07 -0400

    changed param type from string to valueofregions

--- main/fetchers/fetchTimeline.ts
+++ main/fetchers/fetchTimeline.ts
@@ -3,8 +3,9 @@ import { fetchWrapper } from "./fetchWrapper";
 import { FramesResponse } from "../types";
 import { endpoints } from "../constants";
 import url from "url"
+import { ValueOfRegions } from "..";
 
-export function _fetchTimeline(match: number, RIOT_API_REGION: string) {
+export function _fetchTimeline(match: number, RIOT_API_REGION: ValueOfRegions) {
   const MATCH_ENDPOINT = url.resolve(RIOT_API_REGION, endpoints.TIMELINE);
   return axios.get<FramesResponse>(MATCH_ENDPOINT + match,{
     headers: {

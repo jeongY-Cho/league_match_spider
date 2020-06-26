3229c44909847828a57d65ef0c6f5e6dc8e2112c
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-25 20:03:55 -0400

    fixed uri double encoding

--- main/fetchers/fetchAccountInfo.ts
+++ main/fetchers/fetchAccountInfo.ts
@@ -4,9 +4,10 @@ import { ValueOfRegions } from "..";
 import url from "url"
 import Axios, { AxiosResponse } from "axios";
 import { AccountInfo } from "../types"
+import log from "loglevel";
 
 function _fetchAccountInfo(id: string, REGION: ValueOfRegions): Promise<AxiosResponse< AccountInfo>> {
-    let endpoint = encodeURI(url.resolve(REGION, endpoints.ACCOUNT_INFO + id))
+    let endpoint = url.resolve(REGION, endpoints.ACCOUNT_INFO + id)
     return Axios.get(endpoint, {
         headers: {
             "X-Riot-Token": process.env.RIOT_API_KEY

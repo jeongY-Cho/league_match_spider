f649c0404ff5f32464d609d020a068c759c6344e
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-06-26 00:35:43 -0400

    build 1.0.4

--- build/fetchers/fetchTimeline.d.ts
+++ build/fetchers/fetchTimeline.d.ts
@@ -1,3 +1,4 @@
 import { FramesResponse } from "../types";
-export declare function _fetchTimeline(match: number, RIOT_API_REGION: string): Promise<import("axios").AxiosResponse<FramesResponse>>;
-export declare const fetchTimeline: (id: number, region: string) => Promise<import("axios").AxiosResponse<FramesResponse>>;
+import { ValueOfRegions } from "..";
+export declare function _fetchTimeline(match: number, RIOT_API_REGION: ValueOfRegions): Promise<import("axios").AxiosResponse<FramesResponse>>;
+export declare const fetchTimeline: (id: number, region: "https://br1.api.riotgames.com" | "https://eun1.api.riotgames.com" | "https://euw1.api.riotgames.com" | "https://jp1.api.riotgames.com" | "https://kr.api.riotgames.com" | "https://la1.api.riotgames.com" | "https://la2.api.riotgames.com" | "https://na1.api.riotgames.com" | "https://oc1.api.riotgames.com" | "https://tr1.api.riotgames.com" | "https://ru.api.riotgames.com" | "https://americas.api.riotgames.com" | "https://asia.api.riotgames.com" | "https://europe.api.riotgames.com") => Promise<import("axios").AxiosResponse<FramesResponse>>;

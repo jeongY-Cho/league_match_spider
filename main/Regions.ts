bbb82c7bb2b9b36e5b5feb2c4e6d2afe89e78b78
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-03 13:39:58 -0400

    remove comments

--- main/Regions.ts
+++ main/Regions.ts
@@ -1,35 +1,3 @@
-// export interface ERegions {
-//   "BR1" : "BR1",
-//   "EUN1" : "EUN1",
-//   "EUW1" : "EUW1",
-//   "JP1" : "JP1",
-//   "KR" : "KR",
-//   "LA1" : "LA1",
-//   "LA2" : "LA2",
-//   "NA1" : "NA1",
-//   "OC1" : "OC1",
-//   "TR1" : "TR1",
-//   "RU" : "RU",
-//   "AMERICAS" : "AMERICAS",
-//   "ASIA" : "ASIA",
-//   "EUROPE" : "EUROPE",
-// }
-// export type URegions =
-//   | "BR1"
-//   | "EUN1"
-//   | "EUW1"
-//   | "JP1"
-//   | "KR"
-//   | "LA1"
-//   | "LA2"
-//   | "NA1"
-//   | "OC1"
-//   | "TR1"
-//   | "RU"
-//   | "AMERICAS"
-//   | "ASIA"
-//   | "EUROPE";
-
 export interface Regions {
   BR1: "https://br1.api.riotgames.com";
   EUN1: "https://eun1.api.riotgames.com";
